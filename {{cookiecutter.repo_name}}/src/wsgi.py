48f230bd8b23713e60c81777afcad1c79b7ebfcf
Alvaro Lizama Molina <nekrox@gmail.com>
2015-02-24 07:18:08 -0600

    Remove unnecessary python modules and settings

--- {{cookiecutter.repo_name}}/src/wsgi.py
+++ {{cookiecutter.repo_name}}/src/wsgi.py
@@ -27,9 +27,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.project")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
-
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
